42b9dc26d0203d1e4f04fd420ddd5706d0bf9ba7
ViljamiRii <125034560+ViljamiRii@users.noreply.github.com>
2024-04-03 22:31:43 +0300

    Got rid of hashmaps

--- src/runtime/environment.rs
+++ src/runtime/environment.rs
@@ -1,4 +1,3 @@
-use std::collections::{HashMap, HashSet};
 use crate::runtime::values::*;
 
 pub fn create_global_env() -> Environment {
@@ -14,27 +13,27 @@ pub fn create_global_env() -> Environment {
 
 pub struct Environment {
     parent: Option<Box<Environment>>,
-    variables: HashMap<String, RuntimeVal>,
-    constants: HashSet<String>,
+    variables: Vec<(String, RuntimeVal)>,
+    constants: Vec<String>,
 }
 
 impl Environment {
     pub fn new(parent_env: Option<Box<Environment>>) -> Self {
         Self {
             parent: parent_env,
-            variables: HashMap::new(),
-            constants: HashSet::new(),
+            variables: Vec::new(),
+            constants: Vec::new(),
         }
     }
 
     pub fn declare_var(&mut self, varname: String, value: RuntimeVal, constant: bool) -> RuntimeVal {
-        if self.variables.contains_key(&varname) {
+        if self.variables.iter().any(|(name, _)| name == &varname) {
             panic!("Cannot declare variable {}. As it already is defined.", varname);
         }
 
-        self.variables.insert(varname.clone(), value.clone());
+        self.variables.push((varname.clone(), value.clone()));
         if constant {
-            self.constants.insert(varname.clone());
+            self.constants.push(varname.clone());
         }
         value
     }
@@ -47,17 +46,19 @@ impl Environment {
             panic!("Cannot reassign to variable {} as it was declared constant.", varname);
         }
 
-        env.variables.insert(varname, value.clone());
+        if let Some((_, val)) = env.variables.iter_mut().find(|(name, _)| name == &varname) {
+            *val = value.clone();
+        }
         value
     }
     
     pub fn lookup_var(&mut self, varname: &str) -> RuntimeVal {
         let env = self.resolve(varname);
-        env.variables.get(varname).unwrap().clone()
+        env.variables.iter().find(|(name, _)| name == &varname).unwrap().1.clone()
     }
     
     pub fn resolve(&mut self, varname: &str) -> &mut Self {
-        if self.variables.contains_key(varname) {
+        if self.variables.iter().any(|(name, _)| name == &varname) {
             return self;
         }
     
